b6cf82d2bce9d55228ccb3d077e0765bcbfacbdf
Aye Aye Myint <ayeayemyint.mm1999@gmail.com>
2025-02-22 11:15:22 +0630

    Edit baseURL

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5173/src/db/',
+  baseURL: "https://myominthu.vercel.app/src/db",
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default {
@@ -15,20 +15,20 @@ export default {
     return apiClient.get(`/data/${id}`);
   },
   createItem(item) {
-    return apiClient.post('/about.json', item);
+    return apiClient.post("/about.json", item);
   },
-  updateData(id,item) {
+  updateData(id, item) {
     return apiClient.put(`/about.json/${id}`, item);
   },
   deleteItem(id) {
     return apiClient.delete(`/items/${id}`);
   },
   email(data) {
-    return axios.post('https://api.emailjs.com/api/v1.0/email/send',{
-      service_id: 'service_wz5032n',
-      template_id: 'template_n1chi5i',
-      user_id: '4ZIjyo8lfHkFnL4LB',
-      template_params: data
+    return axios.post("https://api.emailjs.com/api/v1.0/email/send", {
+      service_id: "service_wz5032n",
+      template_id: "template_n1chi5i",
+      user_id: "4ZIjyo8lfHkFnL4LB",
+      template_params: data,
     });
-  }
+  },
 };
