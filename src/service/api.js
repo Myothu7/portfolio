e78bdad47b920fa582324268648ab431ca75bc1d
Aye Aye Myint <ayeayemyint.mm1999@gmail.com>
2025-02-22 14:34:23 +0630

    testing apiClient

--- src/service/api.js
+++ src/service/api.js
@@ -1,36 +1,47 @@
+// import axios from "axios";
+
+// const apiClient = axios.create({
+//   baseURL: "http://localhost:5173/src/db/",
+//   baseURL: "https://myominthu.vercel.app/src/db/",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+// });
+
+// export default {
+//   getData(url) {
+//     return apiClient.get(url);
+//   },
+//   getItem(id) {
+//     return apiClient.get(`/data/${id}`);
+//   },
+//   createItem(item) {
+//     return apiClient.post("/about.json", item);
+//   },
+//   updateData(id, item) {
+//     return apiClient.put(`/about.json/${id}`, item);
+//   },
+//   deleteItem(id) {
+//     return apiClient.delete(`/items/${id}`);
+//   },
+//   email(data) {
+//     return axios.post("https://api.emailjs.com/api/v1.0/email/send", {
+//       service_id: "service_wz5032n",
+//       template_id: "template_n1chi5i",
+//       user_id: "4ZIjyo8lfHkFnL4LB",
+//       template_params: data,
+//     });
+//   },
+// };
+
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5173/src/db/",
-  // baseURL: "https://myominthu.vercel.app/src/db/",
+  baseURL: "https://myominthu.vercel.app/src/db/",
   headers: {
-    "Content-Type": "application/json", // This tells the server you are sending JSON data
-    Accept: "application/json", // This tells the server you expect JSON data in return
+    "Content-Type": "application/json",
   },
 });
 
-export default {
-  getData(url) {
-    return apiClient.get(url);
-  },
-  getItem(id) {
-    return apiClient.get(`/data/${id}`);
-  },
-  createItem(item) {
-    return apiClient.post("/about.json", item);
-  },
-  updateData(id, item) {
-    return apiClient.put(`/about.json/${id}`, item);
-  },
-  deleteItem(id) {
-    return apiClient.delete(`/items/${id}`);
-  },
-  email(data) {
-    return axios.post("https://api.emailjs.com/api/v1.0/email/send", {
-      service_id: "service_wz5032n",
-      template_id: "template_n1chi5i",
-      user_id: "4ZIjyo8lfHkFnL4LB",
-      template_params: data,
-    });
-  },
-};
+export default apiClient;
--- src/view/About.vue
+++ src/view/About.vue
@@ -294,31 +294,31 @@ export default {
     };
   },
   methods: {
-    async fetchAbout() {
-      try {
-        const response = await http.getData("about.json");
-        this.about = response.data;
-        console.log(url);
-      } catch (error) {
-        console.error("Error fetching items:", error);
-      }
-    },
+    // async fetchAbout() {
+    //   try {
+    //     const response = await http.getData("about.json");
+    //     this.about = response.data;
+    //     console.log(url);
+    //   } catch (error) {
+    //     console.error("Error fetching items:", error);
+    //   }
+    // },
     async fetchSkill() {
       try {
-        const response = await http.getData("skills.json");
+        const response = await http.get("skills.json");
         this.skill = response.data;
       } catch (error) {
         console.error("Error fetching items:", error);
       }
     },
-    async fetchInterests() {
-      try {
-        const response = await http.getData("interests.json");
-        this.interests = response.data;
-      } catch (error) {
-        console.error("Error fetching items:", error);
-      }
-    },
+    // async fetchInterests() {
+    //   try {
+    //     const response = await http.getData("interests.json");
+    //     this.interests = response.data;
+    //   } catch (error) {
+    //     console.error("Error fetching items:", error);
+    //   }
+    // },
   },
   created() {
     this.fetchAbout();
