6652953cd3f8042f055e493befa5baa296470ef2
Aye Aye Myint <ayeayemyint.mm1999@gmail.com>
2025-02-22 11:55:15 +0630

    Edit baseURL

--- src/service/api.js
+++ src/service/api.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 
-// const apiClient = axios.create({
-//   baseURL: "http://localhost:5173/src/db/",
-//   // baseURL: "https://myominthu.vercel.app/src/db/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
 const apiClient = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:5173/src/db/"
-      : "https://myominthu.vercel.app/src/db/",
+  // baseURL: "http://localhost:5173/src/db/",
+  baseURL: "https://myominthu.vercel.app/src/db/",
   headers: {
     "Content-Type": "application/json",
   },
